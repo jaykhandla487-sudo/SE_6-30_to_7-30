cf1cbaa04df2552c738761b431d0c9624cfe8ea0
jaykhandla487-sudo <jaykhandla487@gmail.com>
2025-10-15 18:08:35 +0530

    Add files via upload

---
+++ C programming/13-10/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int i;
+    
+    for(i=1;i<=10;i++)
+    {
+        if(i==5)
+        {
+            break;
+        }
+        printf("\n %d",i);
+    }
+    getch();
+}
